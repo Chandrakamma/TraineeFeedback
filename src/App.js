accc371e83b5b7b668570dd23d921b2ccd9b191f
K Chandra <chandrakamma06@gmail.com>
2024-05-08 23:58:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TraineeFeedback from './TraineeFeedback'; 
+
+function App() {
+  return (
+    <div className="App">
+      <TraineeFeedback />
+    </div>
+  );
+}
+
+export default App;
